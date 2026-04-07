2340c70111a2ebae6df8f19c4ef7d070e367e7c7
333marci <marcimm2004@gmail.com>
2026-04-07 12:57:18 +0200

    documentacion completa

--- docs/arquitectura/script.js
+++
@@ -1,8 +0,0 @@
-// Initialize Mermaid
-mermaid.initialize({
-    startOnLoad: true,
-    theme: 'neutral',
-    securityLevel: 'loose',
-});
-
-console.log('Documentación técnica cargada correctamente.');
